22be0ceff99e9490a78c272133c7542a8697a0a8
AlexandreRouma <alexandre.rouma@gmail.com>
2018-06-12 00:45:00 +0200

    Update user.js

--- js/user.js
+++ js/user.js
@@ -1,10 +1,1 @@
-function displayUser() {
-    let user = [
-        "HAL9000",
-        "DatBoi",
-        "harambe"
-    ];
-    let status = user[Math.floor(Math.random() * user.length)];
-    return status;
-   
-}
+
